c926189f437e07d1e04628018d280e8d8e882eab
signekatt <liljaanton2001@gmail.com>
2018-04-20 22:27:49 +0200

    Shorter

--- background.js
+++ background.js
@@ -1,28 +1,12 @@
 chrome.webRequest.onBeforeRequest.addListener(
 	function callback(details) {
-		if(details.url.search("&disable_polymer=true") != -1) {
-			return;
-		} else {
-			return{redirectUrl: details.url.concat("&disable_polymer=true")};
+		if(details.url.indexOf("disable_polymer") === -1) {
+			if(details.url.indexOf("?") > 0) {
+				return{redirectUrl: details.url.concat("&disable_polymer=1")};
+			} else {
+				return{redirectUrl: details.url.concat("?disable_polymer=1")};
+			}
 		}
 	},
-	{urls: ["https://www.youtube.com/watch*", 
-			"https://www.youtube.com/playlist*",
-			"https://www.youtube.com/results?search_query=*"
-			]},
-	["blocking"]);
-	
-chrome.webRequest.onBeforeRequest.addListener(
-	function callback(details) {
-		if(details.url.search("disable_polymer=true") != -1) {
-			return;
-		} else {
-			return{redirectUrl: details.url.concat("/?disable_polymer=true")};
-		}
-	},
-	{urls: ["https://www.youtube.com/", 
-			"https://www.youtube.com/feed/*",
-			"https://www.youtube.com/user/*",
-			"https://www.youtube.com/channel/*"
-			]},
+	{urls: ["https://www.youtube.com/*"]},
 	["blocking"]);
